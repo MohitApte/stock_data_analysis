769be8aa3f81804c980d1c02543d879dbeae0c6f
MohitApte <mohit.apte4@gmail.com>
2022-10-27 03:59:28 +0530

    second commit

--- stockData.py
+++ stockData.py
@@ -6,19 +6,21 @@ import pandas as pd
 import yfinance as yf  
 from PIL import Image
 import urllib.request
-
+from bsedata.bse import BSE
+import json
+from datetime import date
+from datetime import timedelta
 # Add a title and an image
 
 st.write("""
-# Stock Market Web Application
-**Visually** show data on a stock!
+# Stock Market Analysis
 """)
 
 urllib.request.urlretrieve('https://cpb-us-w2.wpmucdn.com/u.osu.edu/dist/6/44792/files/2017/04/stock-market-3-21gyd1b.jpg', "stock_image")
 
 image = Image.open("stock_image")
 
-st.image(image, use_column_width=True)
+st.image(image, width=500)
 
 # Create a sidebar header
 
@@ -52,8 +54,74 @@ def get_data(symbol, start, end):
     return df
 
 start, end, symbol = get_input()
-
 df = get_data(symbol,start,end)
+
+#Display the Sensex
+today = date.today()
+yesterday = today - timedelta(days = 1)
+senstart = pd.to_datetime(yesterday)
+senend = pd.to_datetime(yesterday)
+
+sen_df = yf.download("^BSESN", senstart, senend)
+
+st.write("""
+# Current Sensex Close Value
+""")
+cur_val = sen_df['Close'][0]
+st.write("# ", cur_val)
+
+st.write("""
+# BSE Sensex Close Price
+""")
+new_start = pd.to_datetime("2000-01-01")
+new_sen_df = yf.download("^BSESN", new_start, senend)
+
+st.line_chart(new_sen_df['Close'])
+
+
+
+# BSE Top Runners and Top Losers
+
+b = BSE()
+tg = b.topGainers()
+tg_data = json.dumps(tg)
+tg_df = pd.read_json(tg_data)
+tl = b.topLosers()
+tl_data = json.dumps(tl)
+tl_df = pd.read_json(tl_data)
+
+st.write("""
+# BSE Top Runners
+""")
+
+st.write(tg_df)
+
+st.write("""
+# BSE Top Losers
+""")
+
+st.write(tl_df)
+st.write("----------------------------------------------------------------------------------")
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:30px !important;
+    color: red;
+}
+.norm-font {
+    font-size:30px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+st.markdown('<p class="big-font">Disclaimer : </p>', unsafe_allow_html=True)
+st.markdown('<p class="norm-font">On Mobile, Open > at the top left for Company Specific Stock Details</p>', unsafe_allow_html=True)
+
+
+
+st.write("# Stock Statistics of "+ symbol)
+
+
 
 #Display the close price
 
@@ -61,6 +129,7 @@ st.header(symbol + " Close Price\n")
 st.line_chart(df['Close'])
 
 #Display the Volume
+
 
 st.header(symbol + " Volume\n")
 st.line_chart(df['Volume'])
